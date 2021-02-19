2253c44397964a409fa7e36e75672611395123ac
Derek <derekcdlin@gmail.com>
2021-02-19 14:00:51 -0500

    creates board w/ holes, hat, and player

--- field.js
+++ field.js
@@ -1,29 +1,70 @@
-const prompt = require('prompt-sync')({sigint: true});
-
-const hat = '^';
+const hat = 'H';
 const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const fieldCharacter = 'F';
+const pathCharacter = 'P';
 
 class Field {
   constructor(field){
     this._field = field;
+    this._side = field.length;
   }
   get field(){
     return this._field;
   }
+  get side(){
+    return this._side;
+  }
   print(){
+    //console print
     for(let i = 0; i < this.field.length; i++){
       console.log(this.field[i].join(''));
     }
   }
-  static generateField(height, width, percentage){
+  toString(){
+    let ret_string = "";
+    for(let i = 0; i < this.field.length; i++){
+      ret_string += this.field[i].join('') + "\n";
+    }
+    return ret_string;
+  }
+  createBoard(){
+    let gameArea = document.getElementById('board');
+    gameArea.innerHTML = "";
+    gameArea.style['grid-template-rows'] = 'repeat(' + this.side + ' , 1fr)';
+    gameArea.style['grid-template-columns'] = 'repeat(' + this.side + ' , 1fr)';
+    
+    for(let x = 0; x < this.side; x++){
+      for(let y = 0; y < this.side; y++){
+        let cell = document.createElement('div');
+        cell.id = `cell${x},${y}`;
+        cell.className = "cell";
+        cell.style['grid-row'] = x+1 + " / span 1";
+        cell.style['grid-column'] = y+1 + " / span 1";
+  
+        if(this.field[x][y] === 'O'){
+          cell.style.backgroundColor = '#222222';
+        }
+        else if(this.field[x][y] === 'H'){
+          cell.style.backgroundColor = 'yellow'
+        }
+        else if(this.field[x][y] === 'P'){
+          cell.style.backgroundColor = 'red';
+        } 
+        gameArea.appendChild(cell);
+      }
+    }
+  }
+  static generateField(size, percentage){
+    if(percentage > 100){
+      percentage *= 100;
+    }
+    
     const field = [];
-    for(let i = 0; i < height; i++){
+    for(let i = 0; i < size; i++){
       const row = [];
-      for(let j = 0; j < width; j++){
+      for(let j = 0; j < size; j++){
         const chance = Math.floor(Math.random() * 100);
-        if(chance > percentage*100){
+        if(chance > percentage){
           row.push(fieldCharacter);
         }
         else{
@@ -35,10 +76,10 @@ class Field {
 
     let hatX, hatY, startX, startY;
     do{
-      hatX = Math.floor(Math.random() * height);
-      hatY = Math.floor(Math.random() * width);
-      startX = Math.floor(Math.random() * height);
-      startY = Math.floor(Math.random() * width);
+      hatX = Math.floor(Math.random() * size);
+      hatY = Math.floor(Math.random() * size);
+      startX = Math.floor(Math.random() * size);
+      startY = Math.floor(Math.random() * size);
     }while(hatX === startX && hatY === startY);
     field[hatX][hatY] = hat;
     field[startX][startY] = pathCharacter;
@@ -46,6 +87,24 @@ class Field {
   }
 }
 
-/* web app side */
+const startButton = document.getElementById('start');
+const resetButton = document.getElementById('reset');
 
+startButton.addEventListener("click", getFormData);
+resetButton.addEventListener("click", reset);
 
+function getFormData(){
+  const data = new FormData(document.querySelector('form'));
+
+  const size = data.get("size");
+  const percentage = data.get("percentage");
+
+  const board = new Field(Field.generateField(size, percentage));
+
+  board.createBoard();
+}
+
+function reset(){
+  let gameArea = document.getElementById('board');
+  gameArea.innerHTML = "";
+}
--- web-app.js
+++ web-app.js
@@ -1,3 +1,28 @@
-function updateTextInput(val){
-    document.getElementById('percValue').innerHTML = val + "%";
+function updatePercInput(val){
+  //document.getElementById('percValue').innerHTML = "changed";
+  var modeLabel = document.getElementById('percValue');
+  if(val == 0){
+    modeLabel.innerHTML = 'Baby';
+  }
+  else if(val == 10){
+    modeLabel.innerHTML = 'Easy';
+  }
+  else if(val == 20){
+    modeLabel.innerHTML = 'Medium';
+  }
+  else if(val == 30){
+    modeLabel.innerHTML = 'Hard';
+  } 
+  else if(val == 40){
+    modeLabel.innerHTML = 'Expert';
+  } 
+  else{
+    modeLabel.innerHTML = 'Impossible';
+  }  
 }
+
+function updateSizeInput(val){
+  var sizeLabel = document.getElementById('sizeValue');
+  sizeLabel.innerHTML = val + " by " + val;  
+}
+
