c01f33f224643e7890a2223a1b4583704ac4917c
Derek <derekcdlin@gmail.com>
2021-02-19 15:56:31 -0500

    finished game

--- field.js
+++ field.js
@@ -7,19 +7,43 @@ class Field {
   constructor(field){
     this._field = field;
     this._side = field.length;
+    this._goal = [];
+    this._start = [];
+    this._position = [];
   }
+  //getter and setter methods
   get field(){
     return this._field;
   }
   get side(){
     return this._side;
   }
+  get goal(){
+    return this._goal;
+  }
+  get start(){
+    return this._start;
+  }
+  get position(){
+    return this._position;
+  }
+  set goal(coord){
+    this._goal = coord;
+  }
+  set start(coord){
+    this._start = coord;
+  }
+  set position(coord){
+    this._position = coord;
+  }
+
   print(){
     //console print
     for(let i = 0; i < this.field.length; i++){
       console.log(this.field[i].join(''));
     }
   }
+
   toString(){
     let ret_string = "";
     for(let i = 0; i < this.field.length; i++){
@@ -27,7 +51,11 @@ class Field {
     }
     return ret_string;
   }
+
   createBoard(){
+    /*
+    description: created the board interface
+    */
     let gameArea = document.getElementById('board');
     gameArea.innerHTML = "";
     gameArea.style['grid-template-rows'] = 'repeat(' + this.side + ' , 1fr)';
@@ -46,14 +74,94 @@ class Field {
         }
         else if(this.field[x][y] === 'H'){
           cell.style.backgroundColor = 'yellow'
+          this.goal = [x, y];
         }
         else if(this.field[x][y] === 'P'){
           cell.style.backgroundColor = 'red';
+          this.start = [x, y];
+          this.position = this.start;
         } 
         gameArea.appendChild(cell);
       }
     }
   }
+
+  movePlayer(direction){
+    /*
+    description: moves player position
+    */
+    if(!this.validateMove(direction)){
+      logger('error', 'bad move')
+      return;
+    }
+
+    const previous = [this.position[0], [this.position[1]]];
+    if(direction === 'up'){
+      this.position[0]--;
+    }
+    else if(direction === 'down'){
+      this.position[0]++;
+    }
+    else if(direction === 'right'){
+      this.position[1]++;
+    }
+    else if(direction === 'left'){
+      this.position[1]--;
+    }
+    
+    let cell = document.getElementById('cell' + this.position[0] + ',' + this.position[1]);
+    let previousCell = document.getElementById('cell' + previous[0] + ',' + previous[1]);
+    previousCell.style.backgroundColor = 'Tomato'
+    if(this.field[this.position[0]][this.position[1]] === 'O'){
+      //lose
+      cell.style.backgroundColor = "DarkRed";
+      let header = document.getElementById('header');
+      let text = document.getElementById('h1-text');
+      text.innerHTML = 'You fell in a hole!';
+      header.style.backgroundColor = 'DarkRed';
+      document.querySelector('body').removeEventListener('keydown', keyPressed);
+    }
+    else if(this.field[this.position[0]][this.position[1]] === 'H'){
+      //win
+      cell.style.backgroundColor = "DarkOrange";
+      let text = document.getElementById('h1-text');
+      let header = document.getElementById('header');
+      text.innerHTML = 'You found your hat!';
+      header.style.backgroundColor = 'DarkOrange';
+      document.querySelector('body').removeEventListener('keydown', keyPressed);
+    }
+    else{
+      cell.style.backgroundColor = 'red';
+    }
+  }
+
+  validateMove(direction){
+    /*
+    description: checks if player can move in that direction
+    */
+    if(direction === 'up'){
+      if(this.position[0] == 0){
+        return false;
+      }
+    }
+    else if(direction === 'down'){
+      if(this.position[0] == this.side-1){
+        return false;
+      }
+    }
+    else if(direction === 'right'){
+      if(this.position[1] == this.side-1){
+        return false;
+      }
+    }
+    else if(direction === 'left'){
+      if(this.position[1] == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
   static generateField(size, percentage){
     if(percentage > 100){
       percentage *= 100;
@@ -87,11 +195,11 @@ class Field {
   }
 }
 
+/* start button */
 const startButton = document.getElementById('start');
-const resetButton = document.getElementById('reset');
-
 startButton.addEventListener("click", getFormData);
-resetButton.addEventListener("click", reset);
+
+let board;
 
 function getFormData(){
   const data = new FormData(document.querySelector('form'));
@@ -99,12 +207,68 @@ function getFormData(){
   const size = data.get("size");
   const percentage = data.get("percentage");
 
-  const board = new Field(Field.generateField(size, percentage));
+  board = new Field(Field.generateField(size, percentage));
 
   board.createBoard();
+  document.getElementById('debug').innerHTML = "";
+  logger('size', board.side);
+  logger('start', board.start);
+  logger('position', board.position);
+  logger('goal', board.goal);
+
+  /* controls */
+  document.querySelector('body').addEventListener('keydown', keyPressed);
 }
+
+function keyPressed(e){
+  let direction;
+  if(e.keyCode == 87 || e.keyCode == 38)//up
+    direction = 'up';
+  else if(e.keyCode == 83 || e.keyCode == 40) //down
+    direction = 'down';
+  else if(e.keyCode == 68 || e.keyCode == 39) //right
+    direction = 'right';
+  else if(e.keyCode == 65 || e.keyCode == 37) //left
+    direction = 'left';
+    
+  //keyPressed(direction);
+  changeLogger('position_log', board.position);
+  board.movePlayer(direction);
+}
+
+/* reset button */
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener("click", reset);
 
 function reset(){
   let gameArea = document.getElementById('board');
   gameArea.innerHTML = "";
+  document.getElementById('debug').innerHTML = "";
+  board.createBoard();
+  document.querySelector('body').addEventListener('keydown', keyPressed);
+  board.position = board.start;
+
+  let header = document.getElementById('header');
+  let text = document.getElementById('h1-text');
+  text.innerHTML = 'Find your hat!';
+  header.style.backgroundColor = 'limegreen';
+
+  /* debug */
+  logger('size', board.side);
+  logger('start', board.start);
+  logger('position', board.position);
+  logger('goal', board.goal);
+}
+
+/* logger */
+function logger(name, data){
+  let loggingArea = document.getElementById('debug');
+  let log = document.createElement('p');
+  log.id = name + '_log';
+  log.innerHTML = name + ": " + data;
+  loggingArea.appendChild(log);
+}
+function changeLogger(name, data){
+  let logLine = document.getElementById(name);
+  logLine.innerHTML = 'position: ' + data;
 }
